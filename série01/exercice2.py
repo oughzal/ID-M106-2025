b673f3eb4084f4d50de3b6c9a809aeb8efe4d260
omar OUGHZAL <omar.oughzal@gmail.com>
2025-03-04 13:55:45 +0000

    commit

---
+++ série01/exercice2.py
@@ -0,0 +1,4 @@
+PU = float(input("Donner le prix unitaire : "))
+Qt = int(input("Donner la quantité : "))
+TTC = PU*Qt*(1+0.2)
+print("TTC = ", TTC)
---
+++ série01/exercice3.py
@@ -0,0 +1,8 @@
+#Entrée
+R = float(input("Donner la rayon : "))
+# //traitement
+PI = 3.14159
+S=PI*R*R
+P=2*PI*R
+# //sortie
+print("S=",S, " ; P=",P)
